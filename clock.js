df38ff738fb20956a800ac23a6a1835c4fe9be01
Julian Cardone <juliancardone1@gmail.com>
2022-11-03 13:56:32 -0400

    finished clock class

---
+++ clock.js
@@ -0,0 +1,21 @@
+class Clock{
+    constructor(){
+        const time = new Date()
+        this.hours = time.getHours();
+        this.minutes = time.getMinutes();
+        this.seconds = time.getSeconds();
+        this.printTime();
+        setInterval(this._tick.bind(this), 1000);
+    }
+
+    printTime(){
+        console.log(`${this.hours}:${this.minutes}:${this.seconds}`);
+    }
+
+    _tick(){
+        this.seconds++;
+        this.printTime();
+    }
+}
+
+const clock = new Clock();
